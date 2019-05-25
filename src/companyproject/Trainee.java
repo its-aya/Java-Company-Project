4f29a2554a4e60b294a5f02efa9e1f34c4419984
its-aya <44321206+its-aya@users.noreply.github.com>
2019-05-25 23:51:12 +0300

    This is a java company project

---
+++ src/companyproject/Trainee.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package companyproject;
+
+/**
+ *
+ * @author mosaabramadan
+ */
+public class Trainee extends Employee {
+    private String endTrainingDate;
+
+    /**
+     * @return the endTrainingDate
+     */
+    public String getEndTrainingDate() {
+        return endTrainingDate;
+    }
+
+    /**
+     * @param endTrainingDate the endTrainingDate to set
+     */
+    public void setEndTrainingDate(String endTrainingDate) {
+        this.endTrainingDate = endTrainingDate;
+    }
+}
